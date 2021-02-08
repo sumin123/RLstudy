1d1e9542714a4d4e02ca0447cda5955bc3c93613
unknown <soomin61@snu.ac.kr>
2021-02-09 05:12:07 +0900

    ppo fixed

--- PolicyGradient/PPO/main.py
+++ PolicyGradient/PPO/main.py
@@ -5,11 +5,12 @@ import numpy as np
 
 def main():
     env = gym.make('CartPole-v1')
-    gamma = 0.99
+    K = 4
     roll_out = 20
     score = 0.0
     print_interval = 20
     model = net()
+    is_render = False
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         for n_epi in range(10000):
@@ -20,30 +21,36 @@ def main():
                 actions = []
                 rewards = []
                 s_primes = []
-                old_probs = []
-                v_preds = []
-                v_preds_next = []
+                prob_a = []
+                done_masks = []
                 for t in range(roll_out):
+                    if is_render:
+                        env.render()
                     a, a_prob, v_pred = model.sample_action(s.reshape(-1, 4))
                     s_prime,  r, done, info = env.step(a[0])
                     states.append(s)
                     actions.append(a)
                     rewards.append(r/100.0)
                     s_primes.append(s_prime)
-                    old_probs.append(a_prob[0][a])
-                    v_preds.append(v_pred)
+                    prob_a.append(a_prob[0][a])
+                    done_mask = 0.0 if done else 1.0
+                    done_masks.append(done_mask)
                     s = s_prime
 
                     score += r
                     if done:
                         break
-                for i in range(len(v_preds) - 1):
-                    v_preds_next.append(float(v_preds[i+1][0]))
-                v_preds_next.append(float(0))
-                model.train(states, np.array(actions).reshape(-1), np.array(old_probs).reshape(-1), rewards, v_preds, v_preds_next)
+
+            for i in range(K):
+                loss = model.train(states, np.array(actions).reshape(-1), np.array(prob_a).reshape(-1), rewards, s_primes, done_masks)
+                #print(loss)
 
             if n_epi % print_interval == 0 and n_epi != 0:
                 print("episode: %s\tscore: %s" % (n_epi, score / print_interval))
+                if score > 200 * print_interval:
+                    is_render = True
                 score = 0
+
+
 if __name__ == '__main__':
     main()
--- PolicyGradient/PPO/net.py
+++ PolicyGradient/PPO/net.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 
+
 class net():
     def __init__(self):
         super(net, self).__init__()
@@ -8,6 +9,7 @@ class net():
         self.gamma = 0.98
         self.lmbda = 0.95
         self.eps = 0.1
+        self.lr = 0.0005
         self.K = 2
 
         self.build_network('net')
@@ -17,7 +19,7 @@ class net():
     def build_network(self, name):
         ob_space = 4
         with tf.variable_scope(name):
-            self.X = tf.placeholder(tf.float32, [None, 4], name='X')
+            self.X = tf.placeholder(tf.float32, [None, ob_space], name='X')
 
             with tf.variable_scope('policy_net'):
                 layer1 = tf.layers.dense(inputs=self.X, units=256, activation=tf.nn.relu)
@@ -30,8 +32,10 @@ class net():
             self.action = tf.argmax(self.action_prob, axis=1)
             self.action_prob_select = tf.reduce_max(self.action_prob, axis=1)
 
+            self.scope = tf.get_variable_scope().name
+
     def build_net_train(self, name):
-        with tf.variable_scope('train_op'):
+        with tf.variable_scope('train_var'):
             self.actions = tf.placeholder(dtype=tf.int32, shape=[None], name='action')
             self.rewards = tf.placeholder(dtype=tf.float32, shape=[None], name='reward')
             self.action_prob_old = tf.placeholder(dtype=tf.float32, shape=[None], name='action_prob_old')
@@ -39,7 +43,7 @@ class net():
 
         with tf.variable_scope('loss_pi'):
             ratio = tf.exp(tf.log(self.action_prob_select) - tf.log(self.action_prob_old))
-            clipped_ratio = tf.clip_by_value(ratio, 1 - 0.2, 1 + 0.2)
+            clipped_ratio = tf.clip_by_value(ratio, 1 - self.eps, 1 + self.eps)
             loss_clip = tf.minimum(tf.multiply(self.gaes, ratio), tf.multiply(self.gaes, clipped_ratio))
             loss_clip = tf.reduce_mean(loss_clip)
 
@@ -49,30 +53,34 @@ class net():
             loss_v = tf.reduce_mean(loss_v)
 
         with tf.variable_scope('loss'):
-            loss = loss_clip - 1 * loss_v
-            loss = -loss
+            self.loss = - loss_clip + 1 * loss_v
 
-        self.optimizer = tf.train.AdamOptimizer(learning_rate=1e-4, epsilon=1e-5)
-        model_var = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=name)
-        self.train_op = self.optimizer.minimize(loss, var_list=model_var)
+        optimizer = tf.train.AdamOptimizer(learning_rate=self.lr, epsilon=1e-5)
+        model_var = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, self.scope)
+        self.train_op = optimizer.minimize(self.loss, var_list=model_var)
 
-    def train(self, states, action, action_prob_old, reward, v_pred, v_pred_next):
-        gaes, td_target = self.get_gaes(reward, v_pred, v_pred_next)
-        tf.get_default_session().run([self.train_op], feed_dict={self.X: states, self.actions: action, self.action_prob_old: action_prob_old, self.gaes: gaes, self.td_target: td_target})
+    def train(self, states, action, action_prob_old, reward, states_next, done_masks):
+        gaes, td_target = self.get_gaes(states, states_next, reward, done_masks)
+        _, loss = tf.get_default_session().run([self.train_op, self.loss], feed_dict={self.X: states, self.actions: action, self.action_prob_old: action_prob_old, self.gaes: gaes, self.td_target: td_target})
+
+        return loss
 
     def sample_action(self, X):
         return tf.get_default_session().run([self.action, self.action_prob, self.v_pred], feed_dict={self.X: X})
 
-    def get_gaes(self, rewards, v_pred, v_pred_next):
+    def get_gaes(self, states, states_next, rewards, done_masks):
         rewards = np.array(rewards)
+        v_pred = tf.get_default_session().run([self.v_pred], feed_dict={self.X: states})
         v_pred = np.array(v_pred).reshape(-1)
-        v_pred_next = np.array(v_pred_next)
-        td_target = rewards + self.gamma * v_pred_next
+        v_pred_next = tf.get_default_session().run([self.v_pred], feed_dict={self.X: states_next})
+        v_pred_next = np.array(v_pred_next).reshape(-1)
+        td_target = rewards + self.gamma * v_pred_next * done_masks
         delta = td_target - v_pred
         gaes = []
-        advantage = 0
+        advantage = 0.0
         for delta_t in delta[::-1]:
-            gaes.append(self.gamma * self.lmbda * advantage + delta_t)
+            advantage = self.gamma * self.lmbda * advantage + delta_t
+            gaes.append(advantage)
         gaes.reverse()
 
         return gaes, td_target
